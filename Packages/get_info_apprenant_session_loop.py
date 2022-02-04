3a7b90893b41bb8b16f41ba03a2af0738d4299da
acapai <andria.c@eronservice.fr>
2022-02-04 18:30:37 +0100

    *Titre commit: Creation des fonctions pour la démarche de boucle sur les pages GAFEO *Changement(s) effectué(s): - Finalisation des boucles sur les worksheet et sur le chromedriver - Finalisation de l'update du google sheet
    TODO : Réussir à trouver un moyen de bien cibler les formations

---
+++ Packages/get_info_apprenant_session_loop.py
@@ -0,0 +1,197 @@
+import unidecode
+import re
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException,TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import time
+import os
+from fuzzywuzzy import process,fuzz
+
+
+
+def get_info_apprenant_session_loop(dic_dataframe,datasheet,path_directory,start_formation):
+    """
+    Robot/ automatisation de récupération d'infos sur le suivi des apprenants dans leur formation
+    """
+
+    def initialize_browser_pagelogin(time_out):
+        if os.path.exists("/Applications/Internet/Google Chrome.app/Contents/MacOS/Google Chrome"):
+                path_google_chrome="/Applications/Internet/Google Chrome.app/Contents/MacOS/Google Chrome"
+        else:
+            path_google_chrome="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+        options = Options()
+        options.binary_location = path_google_chrome
+        options.add_experimental_option("detach", True)
+        # options.add_argument("--headless")
+        # options.add_argument("--disable-gpu")
+        # options.add_experimental_option('prefs', {'download.default_directory':'/Users/acapai/Downloads/'})
+        # options.add_experimental_option('prefs',{"profile.default_content_setting_values.automatic_downloads": 1})
+
+        rel_path="/Applications/chromedriver"
+        PATH = os.path.abspath(rel_path)
+        id="andria.c@eronservice.fr"
+        psswd="Andria-2021!"
+        browser = webdriver.Chrome(options=options,executable_path=PATH)
+        browser.get("https://www.gafeo.fr/my/")
+        browser.find_element(By.ID,'username').send_keys(id)
+        browser.find_element(By.ID,'password').send_keys(psswd)
+        browser.find_element(By.CSS_SELECTOR,"button.btn-primary").click()
+        return browser
+    
+    def page_recherche_formation(browser,time_out):
+        # div_searchbox = WebDriverWait(browser,time_out).until(EC.presence_of_element_located((By.ID,"coursesearchbox")))
+        inputs_field = WebDriverWait(browser,time_out).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,"#coursesearch .coursesearchbox input")))
+        inputs_field[0].send_keys(formation_table)
+        inputs_field[1].click()
+    
+    def page_list_formation(browser,time_out):
+
+        titre_datasheet = formation_df_copy["Formation_name"].iloc[0]
+        element_title_formation_list = WebDriverWait(browser,time_out).until(EC.presence_of_all_elements_located((By.CLASS_NAME,"coursetitle")))
+        list_title_formation = [ elem.text for elem in element_title_formation_list]
+        tuple_title_chosen = process.extractOne(titre_datasheet, list_title_formation, scorer=fuzz.token_sort_ratio)
+        for i,elem in enumerate(element_title_formation_list):
+            titre_html_gafeo = elem.text
+            if titre_html_gafeo.lower().find("old") ==0:
+                continue
+            score_similarity_text= fuzz.token_set_ratio(titre_datasheet,titre_html_gafeo)
+            print(score_similarity_text)
+            if len(element_title_formation_list)==1:
+                formation_to_select =  elem
+            elif score_similarity_text > 90:
+                formation_to_select =  elem
+            elif "v2" in titre_html_gafeo or "v3":
+                formation_to_select =  elem
+            elif tuple_title_chosen[0] == titre_html_gafeo:
+                print(tuple_title_chosen[0])
+                print(titre_html_gafeo)
+                formation_to_select =  elem
+            else:
+                continue
+
+        element_title_formation_list = WebDriverWait(formation_to_select,time_out).until(EC.presence_of_element_located((By.XPATH,"..")))
+        parent_element_title_formation_list = WebDriverWait(element_title_formation_list,time_out).until(EC.presence_of_element_located((By.XPATH,"..")))
+        image_link = WebDriverWait(parent_element_title_formation_list,time_out).until(EC.presence_of_element_located((By.CLASS_NAME,"hovercoursebox")))
+        image_link.send_keys(Keys.RETURN)
+        # action_link.click()
+
+    def page_formation_detail(browser,time_out):
+        div_button_config_course = WebDriverWait(browser,time_out).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,".teacherdash a")))
+        div_button_config_course[0].click()
+    
+    def page_formation_detail_popup(browser,time_out):
+        WebDriverWait(browser,time_out).until(EC.presence_of_element_located((By.LINK_TEXT,"Rapports de session"))).click()
+    
+    def page_details_rapport_prefill_list_apprenant(browser,time_out):
+        dp_down_list_participant = Select(WebDriverWait(browser,time_out).until(EC.presence_of_element_located((By.CSS_SELECTOR,"select[name='userid']"))))
+
+        list_apprenant= formation_df_copy['Apprenant_Normalize'].tolist()
+        for i,element in enumerate(dp_down_list_participant.options):
+            print(element.get_attribute("value"))
+            print(element.get_attribute("text"))
+            # element_normalize=re.sub(r'[^a-zA-Z0-9]','',unidecode.unidecode(element.get_attribute("text")).lower().replace(' ', ''))
+            element_normalize=unidecode.unidecode(element.get_attribute("text")).lower()
+            print("element_normalize:",element_normalize)
+            print("list_apprenant:",list_apprenant)
+            ratios = process.extract(element_normalize,list_apprenant,limit=len(list_apprenant))
+            print(ratios)
+            highest = process.extractOne(element_normalize,list_apprenant)
+            print("Highest",highest)
+            index= [x for x, y in enumerate(list_apprenant) if y == highest[0]]
+            if highest[1]>75:
+                print("Ratio",highest[1])
+                print("index",index[0])
+                formation_df_copy.iloc[index[0],formation_df_copy.columns.get_loc("Ratio_Fuzzy")]=highest[1]
+                formation_df_copy.iloc[index[0],formation_df_copy.columns.get_loc("Name_Fuzzy")]=highest[0]
+                formation_df_copy.iloc[index[0],formation_df_copy.columns.get_loc("option_value")]=element.get_attribute("value")
+                formation_df_copy.iloc[index[0],formation_df_copy.columns.get_loc("Apprenant_Gafeo")]=element.get_attribute("text")
+        return 
+
+    def get_details_suivi_formations_apprenant(browser,time_out):
+        for ind in formation_df_copy.index:
+            value = formation_df_copy["option_value"][ind]
+            if formation_df_copy["Apprenant_Gafeo"][ind] !="":
+                lastname=formation_df_copy["Apprenant_Gafeo"][ind].split()[0]
+                firstname=formation_df_copy["Apprenant_Gafeo"][ind].split()[1]
+                print(firstname+" "+lastname)
+                print("value option",value)
+            if value != "":
+                dp_down_list_participant = Select(WebDriverWait(browser,time_out).until(EC.presence_of_element_located((By.CSS_SELECTOR,"select[name='userid']"))))
+                dp_down_list_participant.select_by_value(value)
+                opt_name = WebDriverWait(browser,time_out).until(EC.presence_of_element_located((By.CSS_SELECTOR,"input[name='go_btn']")))
+                opt_name.send_keys(Keys.RETURN)
+                WebDriverWait(browser,time_out).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,".userinfobox h1"),lastname))
+                info_pourcentage_list = WebDriverWait(browser,time_out).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,".mandatory-items div")))
+                info_pourcentage_text = info_pourcentage_list[2].text
+                pourcentage_only=re.findall(r"^.*?\([^\d]*(\d+)[^\d]*\).*$",info_pourcentage_text)
+                print("%:",pourcentage_only[0])
+
+                info_last_access = WebDriverWait(browser,time_out).until(EC.presence_of_element_located((By.CSS_SELECTOR,"#sample-lastcourseaccess .sample-value")))
+                print("info_last_access",info_last_access.text)
+                formation_df_copy.loc[ind,'Dernier Accès'] = info_last_access.text
+                formation_df_copy.loc[ind,'Pourcentage']=pourcentage_only[0]
+
+    def update_datasheet():
+        # print(datasheet.loc[pd_copy.index.values,"Dernier Accès"])
+        # print(datasheet.loc[pd_copy.index.values,"Apprenant"])
+        datasheet.loc[formation_df_copy.index.values,"Dernier Accès"] = formation_df_copy.loc[formation_df_copy.index.values,"Dernier Accès"]
+        datasheet.loc[formation_df_copy.index.values,"Pourcentage"] = formation_df_copy.loc[formation_df_copy.index.values,"Pourcentage"]
+
+    def save_step_process(nb_time_loop,path_directory):
+        datasheet.to_pickle(os.path.join(path_directory,"datasheet_loop_"+str(nb_time_loop)))
+
+
+    formation_key_dic=list(dic_dataframe.keys())
+    time_out=30
+    nb_time_loop=start_formation
+    formation_key_dic_start=formation_key_dic[start_formation:]
+    print(formation_key_dic_start)
+    error=False
+    for formation_table in formation_key_dic_start:
+        print(formation_table)
+        print(dic_dataframe[formation_table]["Formation_name"])
+        formation_df_copy = dic_dataframe[formation_table]# A changer
+        formation_df_copy["Ratio_Fuzzy"]=""
+        formation_df_copy["Name_Fuzzy"]=""
+        formation_df_copy["option_value"]=""
+        # print(formation_key_dic)
+        error=False
+        try:
+            browser = initialize_browser_pagelogin(time_out)
+            page_recherche_formation(browser,time_out)
+            page_list_formation(browser,time_out)
+            page_formation_detail(browser,time_out)
+            page_formation_detail_popup(browser,time_out)
+            page_details_rapport_prefill_list_apprenant(browser,time_out)
+            get_details_suivi_formations_apprenant(browser,time_out)
+        except NoSuchElementException:
+            print("There was an error, no such element")
+            error=True
+            browser.quit()
+        except TimeoutException:
+            error=True
+            browser.quit()
+        else:
+            print("There were no errors.")
+        finally:
+            print("Process completed.")
+            if error:
+                time.sleep(5)
+                browser.quit()
+            else:
+                update_datasheet()
+                nb_time_loop+=1
+                save_step_process(nb_time_loop,path_directory)
+                browser.quit()
+                time.sleep(10)
+
+
+    
+    return datasheet
+
+
--- pygsheet_connect.py
+++ pygsheet_connect.py
@@ -1,27 +1,91 @@
-import asyncio
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-
+import unidecode
+import re
+import os
+import pandas as pd
 import pygsheets
-
+from Packages.get_info_apprenant_session_loop import get_info_apprenant_session_loop
+from Packages.update_googlesheet_data import update_workseet_suivi_eron
 gc = pygsheets.authorize(client_secret='/Users/acapai/Documents/Git/Espace-test/Scrapping-With-Python/Scrapping_Url_GAFEO/Oauth_gg/code_secret_client_95743482524-gj2mnoav9naiqt454ggvt71r28r4n3dk.apps.googleusercontent.com.json')
 
 # Suivi Eron 2022 (derniere version)
 #sh = gc.open_by_key('13VqSH8KjAzB3-mroVhtUJjXgO2Gs31UtpqdehiLMyRs')
 
 # DUPLICATA Suivi Eron 2022 (derniere version)
-#sh = gc.open_by_key('13VqSH8KjAzB3-mroVhtUJjXgO2Gs31UtpqdehiLMyRs')
 sh = gc.open_by_key('1Ix4xc_kJPrIBXQL8JmGVz_XNnY4t7AMHdHvmyVlExiA')
 
-wksheet_depart_inf_01_01 = sh[4]
-col_name_apprenant = wksheet_depart_inf_01_01.get_col(4)
+for sheetNumber in range(5,22):
+    print(sheetNumber)
+    wksheet = sh[sheetNumber]
 
-print(col_name_apprenant)
-print(wksheet_depart_inf_01_01.title)
-print(sh.title)
 
-cells = wksheet_depart_inf_01_01.get_all_values(include_empty_rows=False, include_tailing_empty=False, returnas='cells')
+    col_formation_raw = wksheet.get_col(8)
+
+    # print(col_name_apprenant)
+    print(wksheet.title)
+    print(sh.title)
+
+    try:
+        path_directory=os.path.join(os.path.dirname(__file__),"data",wksheet.title.replace("/","-").replace("(","").replace(")","").replace(" ","-"))
+        print(path_directory)
+        os.makedirs(path_directory)
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    if len(os.listdir(path_directory) ) == 0:
+        max_value=0
+        col_name_apprenant = wksheet.get_col(9)
+        col_apprenant = [string for string in col_name_apprenant if string ]
+
+        col_formation_name=[]
+        col_formation_raw = wksheet.get_col(8)
+        col_formation = [ string.split("-")[0] for string in col_formation_raw if string]
+        for string in col_formation_raw:
+            if string and len(string.split("-"))==3:
+                col_formation_name.append(string.split("-")[2])
+            elif string!="":
+                col_formation_name.append(string)
+            else:
+                continue
+        print(col_formation)
+        print(col_formation_name)
+
+
+        datasheet = pd.DataFrame(list(zip(col_formation[2:],col_formation_name[2:],col_apprenant[2:])),columns=['Formation','Formation_name','Apprenant'])
+        datasheet['Dernier Accès'] = ''
+        datasheet['Pourcentage']=''
+        datasheet['Apprenant_Normalize']=''
+        datasheet["Apprenant_Gafeo"]=""
+
+        #Normalize name 
+        for index,row in datasheet.iterrows():
+            print(unidecode.unidecode(row['Apprenant']))
+            print(row['Apprenant'].lower())
+            print(unidecode.unidecode(row['Apprenant'].lower()))
+            print(re.sub(r'[^a-zA-Z0-9]','',unidecode.unidecode(row['Apprenant'].lower().replace(' ', ''))))
+            # row['Apprenant_Normalize']= re.sub(r'[^a-zA-Z0-9]','',unidecode.unidecode(row['Apprenant']).lower().replace(' ', ''))
+            row['Apprenant_Normalize']= unidecode.unidecode(row['Apprenant']).lower()
+        
+        formation_unique= datasheet.Formation.unique().tolist()
+
+    else:
+        files_name = os.listdir(path_directory)
+        list_number = [ int(f.split("_")[-1]) for f in files_name]
+        max_value = max(list_number)
+        max_index = list_number.index(max_value)
+        datasheet = pd.read_pickle(os.path.join(path_directory,files_name[max_index]))  
+        formation_unique= datasheet.Formation.unique().tolist()
+
+        col_formation_raw = wksheet.get_col(8)
+        col_formation = [ string.split("-")[0] for string in col_formation_raw if string]
+        unique_formation_from_googlesheet=set(col_formation[2:])
+
+    if len(unique_formation_from_googlesheet) != max_value:
+        dic_dataframe={}
+        for formation in formation_unique:
+            dic_dataframe[formation]=datasheet[datasheet["Formation"]==formation]
+
+        updated_datasheet = get_info_apprenant_session_loop(dic_dataframe,datasheet,path_directory,start_formation=max_value)
+        update_workseet_suivi_eron(wksheet,updated_datasheet)
+    else:
+        update_workseet_suivi_eron(wksheet,datasheet)
